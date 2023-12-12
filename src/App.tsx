434d47f4e7c4936ffd125521480769a4617b8099
Phonng <heartgold1405@gmail.com>
2023-12-12 09:36:30 +0700

    feat: logic (#2)
    * feat: logic (WIP)
    
    * feat: logic
    
    ---------
    
    Co-authored-by: phong.vh <phong.vh@kiotviet.com>

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import {
   useSortable,
 } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
+import { arrayMove, insertAtIndex, removeAtIndex } from "./utils/array";
 type MenuItem = Required<MenuProps>["items"][number];
 // const SortableItem = ({ id, children }) => {
 //   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -34,6 +36,19 @@ type MenuItem = Required<MenuProps>["items"][number];
 //     </div>
 //   );
 // };
+const moveBetweenContainers = (
+  items,
+  activeContainer,
+  activeIndex,
+  overContainer,
+  overIndex,
+  item
+) => {
+  return {
+    ...items,
+    [overContainer]: insertAtIndex(items[overContainer], overIndex, item),
+  };
+};
 const SortableItem = (props) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: props.id });
@@ -58,8 +73,9 @@ const SortableItem = (props) => {
     </div>
   );
 };
+const initialSortItemKeys: string[] = ["mail", "mail1", "mail2", "mail3"];
 
-const items2: MenuItem[] = [
+const initialItems: MenuItem[] = [
   {
     label: (
       <SortableItem id="mail" key="mail">
@@ -74,7 +90,7 @@ const items2: MenuItem[] = [
         Navigation 2
       </SortableItem>
     ),
-    key: "app1",
+    key: "mail1",
   },
   {
     label: (
@@ -82,7 +98,7 @@ const items2: MenuItem[] = [
         Navigation 3
       </SortableItem>
     ),
-    key: "mail1",
+    key: "mail2",
   },
   {
     label: (
@@ -90,17 +106,21 @@ const items2: MenuItem[] = [
         Navigation 4
       </SortableItem>
     ),
-    key: "app",
+    key: "mail3",
   },
 ];
-const Menu = ({ items }) => {
+const Menu = ({ items, sortItemKeys }) => {
   const id = "menu2";
   const { setNodeRef } = useDroppable({ id });
 
   return (
-    <SortableContext id={id} items={items} strategy={rectSortingStrategy}>
+    <SortableContext
+      id={id}
+      items={sortItemKeys}
+      strategy={rectSortingStrategy}
+    >
       <div ref={setNodeRef}>
-        <Dropdown menu={{ items: items2 }} trigger={["click"]}>
+        <Dropdown menu={{ items }} trigger={["click"]}>
           <a onClick={(e) => e.preventDefault()}>
             <Button>Click me</Button>
           </a>
@@ -110,9 +130,8 @@ const Menu = ({ items }) => {
   );
 };
 function App() {
-  const items = {
-    group1: ["mail", "mail1", "mail2", "mail3"],
-  };
+  const [sortItemKeys, setSortItemKeys] = useState(initialSortItemKeys);
+  const [items, setItems] = useState(initialItems);
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -120,8 +139,24 @@ function App() {
     })
   );
   const handleDragEnd = ({ active, over }) => {
-    console.log(123123, { active, over });
+    if (!over) {
+      return;
+    }
+
+    if (active.id !== over.id) {
+      const activeIndex = active.data.current.sortable.index;
+      const overIndex = over.data.current?.sortable.index || 0;
+      const newSortedItems = arrayMove(sortItemKeys, activeIndex, overIndex);
+      setSortItemKeys(newSortedItems);
+      setItems((items) => {
+        const newItems = newSortedItems.map((value) =>
+          items.find((obj) => obj.key === value)
+        );
+        return newItems;
+      });
+    }
   };
+
   const containerStyle = {
     display: "flex",
     justifyContent: "center",
@@ -133,7 +168,7 @@ function App() {
   return (
     <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
       <div style={containerStyle}>
-        <Menu items={items.group1} />
+        <Menu items={items} sortItemKeys={sortItemKeys} />
       </div>
     </DndContext>
   );
